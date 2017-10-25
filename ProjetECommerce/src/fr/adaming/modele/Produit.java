3b155cf688bea16e4d29f055c14266b9fa22a5ff
inti0236 <inti0236@192.168.1.171>
2017-10-25 17:11:26 +0200

    benjamin: correction table

--- ProjetECommerce/src/fr/adaming/modele/Produit.java
+++ ProjetECommerce/src/fr/adaming/modele/Produit.java
@@ -2,6 +2,7 @@ package fr.adaming.modele;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Produit {
 	private String description;
 	private double prix;
 	private int quantite;
+	@Column(columnDefinition="TINYINT(1)")
 	private boolean selectionne;
 	
 	// Association avec la catégorie
