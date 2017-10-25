9413417580ba39a194dbc44044ca8ceedf196290
inti0236 <inti0236@192.168.1.171>
2017-10-25 15:07:41 +0200

    commit le correge

--- ProjetECommerce/src/fr/adaming/service/AdministrateurServiceImpl.java
+++ ProjetECommerce/src/fr/adaming/service/AdministrateurServiceImpl.java
@@ -1,1 +1,31 @@
+package fr.adaming.service;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+
+import fr.adaming.dao.IAdministrateurDao;
+import fr.adaming.modele.Administrateur;
+
+@Stateful
+public class AdministrateurServiceImpl implements IAdministrateurService{
+	
+	@EJB
+	private IAdministrateurDao administrateurDao ;
+
+	public IAdministrateurDao getAdministrateurDao() {
+		return administrateurDao;
+	}
+
+	public void setAdministrateurDao(IAdministrateurDao administrateurDao) {
+		this.administrateurDao = administrateurDao;
+	}
+
+	@Override
+	public Administrateur connexion(Administrateur a) {
+		return administrateurDao.connexion(a);
+	}
+	
+	
+
+}
 
---
+++ ProjetECommerce/src/fr/adaming/service/IAdministrateurService.java
@@ -0,0 +1,11 @@
+package fr.adaming.service;
+
+import fr.adaming.modele.Administrateur;
+
+public interface IAdministrateurService {
+
+	
+	public Administrateur connexion(Administrateur a);
+
+
+}
